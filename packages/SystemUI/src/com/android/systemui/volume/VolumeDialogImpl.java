ca19f3f76dca130b2471453cf503ffa74074d58d
Lucchetto <lucchetto.tie@live.com>
2019-02-14 15:00:12 +0100

    [SQUASHED] Gradient Shit [1/2]
    Apply gradient to some device default UI elements
    
    Edits for Android 10: use values-night for dark gradients
    
    Change-Id: I256b9a6ad762882f487ea57c89a94be20194b9b6
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Tweak some UI values
    
    Looks nicer with our theme
    
    Change-Id: Ie2a80dc4e708528bf8e728cde0534a6f711523a7
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Update brightness slider UI
    
    Change-Id: I52b482c1b3b17413e42d095d0683fc4cfdd91bd4
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Apply Gradient style to settings
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Make popup menus more rounded
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Use our switch style for Theme.DeviceDefault.Light
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Tint active volume bar with gradient
    
    Change-Id: I6528a1d06daf66c3b5f83fb968f618d886495d1f
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Remove secondaryProgress from our progress and seek bars
    
    Change-Id: I5d0129061de87d96384dd02203c3340f96cdce9c
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1363,10 +1363,10 @@ public class VolumeDialogImpl implements VolumeDialog,
         final int alpha = useActiveColoring
                 ? Color.alpha(tint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.secondaryContentAlpha);
-        if (tint == row.cachedTint && mExpanded) return;
-        row.slider.setProgressTintList(tint);
+        final ColorStateList progressTint = useActiveColoring ? null : tint;
+        if (tint == row.cachedTint) return;
+        row.slider.setProgressTintList(progressTint);
         row.slider.setThumbTintList(tint);
-        row.slider.setProgressBackgroundTintList(tint);
         row.slider.setAlpha(((float) alpha) / 255);
         row.icon.setImageTintList(tint);
         row.icon.setImageAlpha(alpha);
